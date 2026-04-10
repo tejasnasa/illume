191ca15c7e37538196bce07766117c9044a8395c
tejasnasa <tejasnasa2005@gmail.com>
2026-04-10 13:08:19 +0530

    feat: add Button with loading spinner

---
+++ client/src/app/test/page.tsx
@@ -0,0 +1,9 @@
+import Button from "@/ui/Button";
+
+export default function Test() {
+    return (
+        <div>
+            <Button size="lg">Click this</Button>
+        </div>
+    );
+}
---
+++ client/src/ui/Button.tsx
@@ -0,0 +1,47 @@
+import { Spinner } from "@/icons/Spinner";
+
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  size?: "xs" | "sm" | "md" | "lg";
+  ratio?: number;
+  loading?: boolean;
+};
+
+const sizes = {
+  xs: "px-2.5 py-1 text-xs",
+  sm: "px-4 py-2 text-sm",
+  md: "h-11 px-5 text-sm",
+  lg: "h-12 px-6 text-base",
+};
+
+const iconSizes = {
+  xs: "h-2.5 w-2.5",
+  sm: "h-3 w-3",
+  md: "h-4 w-4",
+  lg: "h-5 w-5",
+};
+
+export default function Button({
+  className = "",
+  size = "md",
+  ratio,
+  style,
+  loading = false,
+  ...props
+}: Props) {
+  const base = sizes[size];
+
+  return (
+    <button
+      {...props}
+      style={{
+        ...style,
+        ...(ratio && { aspectRatio: ratio }),
+      }}
+      disabled={loading || props.disabled}
+      className={`bg-(--primary) text-(--primary-foreground) hover:bg-(--primary)/90 rounded-xl transition-all duration-200 active:translate-y-0.5 inline-flex items-center justify-center gap-2 font-medium disabled:bg-(--primary)/40 disabled:cursor-default ${base} ${ratio ? "px-0" : ""} ${className}`}
+    >
+      {loading && <Spinner className={iconSizes[size]} />}
+      {props.children}
+    </button>
+  );
+}
