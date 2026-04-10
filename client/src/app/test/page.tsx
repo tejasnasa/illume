ca6fb9b939ffc35ce20d32af72d9b88ad1b7f24b
tejasnasa <tejasnasa2005@gmail.com>
2026-04-10 15:19:51 +0530

    feat: add Skeleton component with shimmer animation

--- client/src/app/test/page.tsx
+++ client/src/app/test/page.tsx
@@ -3,12 +3,13 @@
 import Button from "@/components/ui/Button";
 import Input from "@/components/ui/Input";
 import Modal from "@/components/ui/Modal";
+import { Skeleton } from "@/components/ui/Skeleton";
 import Textarea from "@/components/ui/Textarea";
 import { toast } from "@/lib/use-toast";
 
 export default function Test() {
   return (
-    <div className="flex-col flex w-min gap-5 m-5">
+    <div className="flex-col flex gap-5 m-5">
       <Button
         size="md"
         onClick={() =>
@@ -27,6 +28,8 @@ export default function Test() {
         <div>Hello!</div>
         <Button>click this fr</Button>
       </Modal>
+      <Skeleton className="w-64 h-64" />
+      <Skeleton className="w-48 h-6" />
     </div>
   );
 }
