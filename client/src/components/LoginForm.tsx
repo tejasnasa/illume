39e213ff5437c4fffa063119a40f231224ed4f24
tejasnasa <tejasnasa2005@gmail.com>
2026-04-12 21:24:31 +0530

    feat: integrate react-hook-form into login form

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,10 +1,28 @@
+import { loginSchema } from "@/validations/schema";
 import Link from "next/link";
+import { UseFormRegister } from "react-hook-form";
+import z from "zod";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 
-export default function LoginForm() {
+interface LoginFormProps {
+  register: UseFormRegister<z.infer<typeof loginSchema>>;
+  firstError?: string;
+  isSubmitting: boolean;
+  onSubmit: (e: React.SubmitEvent) => void;
+}
+
+export default function LoginForm({
+  register,
+  firstError,
+  isSubmitting,
+  onSubmit,
+}: LoginFormProps) {
   return (
-    <form className="bg-transparent w-[55%] p-8 rounded-sm mb-24">
+    <form
+      className="bg-transparent w-[55%] p-8 rounded-sm mb-24"
+      onSubmit={onSubmit}
+    >
       <h2 className="text-2xl font-bold mb-1 text-center">
         Login to your account
       </h2>
@@ -16,21 +34,31 @@ export default function LoginForm() {
         <label htmlFor="email" className="text-sm">
           Email
         </label>
-        <Input id="email" type="email" placeholder="tejas@example.com" />
+        <Input
+          id="email"
+          type="email"
+          placeholder="tejas@example.com"
+          {...register("email")}
+        />
       </div>
 
       <div className="flex flex-col my-4 gap-1">
         <label htmlFor="password" className="text-sm">
           Password
         </label>
-        <Input id="password" type="password" placeholder="*********" />
+        <Input
+          id="password"
+          type="password"
+          placeholder="*********"
+          {...register("password")}
+        />
       </div>
 
-      <p className="text-(--destructive) my-2 text-sm hidden">
-        This is an error message.
-      </p>
+      {firstError && (
+        <p className="text-(--destructive) my-2 text-sm hidden">{firstError}</p>
+      )}
 
-      <Button className="w-full my-4" size="sm">
+      <Button className="w-full my-4" size="sm" loading={isSubmitting}>
         LOGIN
       </Button>
 
