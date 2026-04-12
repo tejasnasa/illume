58e873b7c334804836b897a7d793de4de38ffbb2
tejasnasa <tejasnasa2005@gmail.com>
2026-04-12 21:32:32 +0530

    feat: create useSignupForm hook

---
+++ client/src/app/hooks/useSignupForm.ts
@@ -0,0 +1,53 @@
+import { signupSchema } from "@/validations/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+export function useSignupForm() {
+  const router = useRouter();
+  const form = useForm<z.infer<typeof signupSchema>>({
+    resolver: zodResolver(signupSchema),
+    defaultValues: { name: "", email: "", password: "" },
+  });
+
+  const { name, email, password, root } = form.formState.errors;
+  const firstError =
+    name?.message || email?.message || password?.message || root?.message;
+
+  const onSubmit = form.handleSubmit(async (data) => {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/signup`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+          body: JSON.stringify(data),
+        },
+      );
+
+      if (!res.ok) {
+        const error = await res.json();
+        throw new Error(
+          error.message ?? "Something went wrong. Please try again.",
+        );
+      }
+
+      router.push("/dashboard");
+    } catch (error) {
+      form.setError("root", {
+        message:
+          (error as { message?: string }).message ??
+          "Something went wrong. Please try again.",
+      });
+    }
+  });
+
+  return {
+    register: form.register,
+    firstError,
+    isSubmitting: form.formState.isSubmitting,
+    onSubmit,
+  };
+}
