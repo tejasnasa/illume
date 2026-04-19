acd6b7d0ff721c0076f377247e94fbd1632e4d3d
tejasnasa <tejasnasa2005@gmail.com>
2026-04-19 13:33:13 +0530

    feat: create ownership api calls

---
+++ client/src/api/ownership.ts
@@ -0,0 +1,42 @@
+import Ownership, { Silo } from "@/types/ownership";
+import { headers } from "next/headers";
+
+export async function GetOwnership(
+  id: string,
+  page: number,
+  page_size: number,
+): Promise<Ownership> {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/repository/${id}/ownership?page=${page}&page_size=${page_size}`,
+    {
+      headers: { cookie: (await headers()).get("cookie") ?? "" },
+      cache: "no-store",
+    },
+  );
+
+  if (!res.ok) throw new Error("Failed to fetch repo ownership");
+
+  const data = await res.json();
+
+  return data;
+}
+
+export async function GetOwnershipSilos(
+  id: string,
+  page: number,
+  page_size: number,
+): Promise<Silo> {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/repository/${id}/ownership/silos?page=${page}&page_size=${page_size}`,
+    {
+      headers: { cookie: (await headers()).get("cookie") ?? "" },
+      cache: "no-store",
+    },
+  );
+
+  if (!res.ok) throw new Error("Failed to fetch repo ownership");
+
+  const data = await res.json();
+
+  return data;
+}
