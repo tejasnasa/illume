daeb500f3f19c9aa8e6ca88e3d43d09a0eb9e8c2
tejasnasa <tejasnasa2005@gmail.com>
2026-06-02 23:01:34 +0530

    fix: Fix logout flow

--- client/src/actions/logout.ts
+++ client/src/actions/logout.ts
@@ -1,19 +1,10 @@
 "use server";
 
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function logoutAction() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/logout`,
-    {
-      method: "POST",
-      credentials: "include",
-    },
-  );
-
-  if (!res.ok) {
-    throw new Error("Failed to logout");
-  }
-
+  const cookieStore = await cookies();
+  cookieStore.delete("access_token");
   redirect("/login");
 }
